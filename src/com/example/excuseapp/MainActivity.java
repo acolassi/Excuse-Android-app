61846a0e8dca1c56d1f4f1631c512553c7c685a7
Aaron N. Rudolph <aanrudolph2@gmail.com>
2016-04-04 15:06:54 -0400

    Added code to react to buttons in UI.

--- src/com/example/excuseapp/MainActivity.java
+++ src/com/example/excuseapp/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.excuseapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements OnClickListener
+{
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.excuseviewer);
+		
+		((Button)findViewById(R.id.low_priority)).setOnClickListener(this);
+		((Button)findViewById(R.id.med_priority)).setOnClickListener(this);
+		((Button)findViewById(R.id.high_priority)).setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		switch(v.getId())
+		{
+		case R.id.low_priority:
+			Toast.makeText(this, "Low Priority Excuse", Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.med_priority:
+			Toast.makeText(this, "Medium Priority Excuse", Toast.LENGTH_SHORT).show();
+			break;
+		case R.id.high_priority:
+			Toast.makeText(this, "High Priority Excuse", Toast.LENGTH_SHORT).show();
+			break;
+		}
+		
 	}
 }
