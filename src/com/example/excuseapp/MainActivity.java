955f9458525c1c4de4d3a04c51e398ccee390048
Aaron N. Rudolph <aanrudolph2@gmail.com>
2016-04-11 15:04:43 -0400

    Added Excuse Delay. Added Notification Sound.

--- src/com/example/excuseapp/MainActivity.java
+++ src/com/example/excuseapp/MainActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
@@ -14,8 +16,8 @@ import android.widget.Button;
 public class MainActivity extends Activity implements OnClickListener
 {
 	ExcuseGenerator ex;
-	
 	NotificationManager mgr;
+	Context c;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -30,12 +32,14 @@ public class MainActivity extends Activity implements OnClickListener
 		ex = new ExcuseGenerator(this);
 		
 		mgr = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+		
+		c = this;
 	}
 
 	@Override
 	public void onClick(View v)
 	{
-		String excuse = null;
+		final String excuse;
 		switch(v.getId())
 		{
 		case R.id.low_priority:
@@ -47,15 +51,26 @@ public class MainActivity extends Activity implements OnClickListener
 		case R.id.high_priority:
 			excuse = ex.getExcuse(1);
 			break;
+		default:
+			excuse = null;
 		}
 		
-		
-		Notification alert = new NotificationCompat.Builder(this)
-		.setContentTitle("Alert")
-		.setContentText(excuse)
-		.setSmallIcon(android.R.drawable.ic_dialog_alert)
-		.build();
-		
-		mgr.notify(32, alert);
+		Handler handler = new Handler();
+		handler.postDelayed(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				Notification alert = new NotificationCompat.Builder(c)
+						.setContentTitle("Alert")
+						.setContentText(excuse)
+						.setSmallIcon(android.R.drawable.ic_dialog_alert)
+						.setSound(Uri.parse("android.resource://com.example.excuseapp/raw/kimpossible"))
+						.setPriority(Notification.PRIORITY_HIGH)
+						.build();
+						
+						mgr.notify(32, alert);
+			}
+		}, 300000);
 	}
 }
